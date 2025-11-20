9812cb646f7a5c51b80ab6cc3e48504e4ec7b9a2
Sai Sanapureddy <ss2644@cornell.edu>
2025-11-20 17:10:48 -0500

    uibuild

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
+require('dotenv').config()
 const express = require('express')
+const Person = require('./models/persons')
 const morgan = require('morgan')
 const cors = require('cors')
 
@@ -18,35 +20,14 @@ morgan.token('body', (req) => {
 // Use a format that includes the :body token
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello from persons svc</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
