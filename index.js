89de976f4517f6f67d2e8f50d470999dc3a38f97
Sai Sanapureddy <ss2644@cornell.edu>
2025-11-20 17:23:24 -0500

    uibuild

--- index.js
+++ index.js
@@ -41,18 +41,23 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    const person = persons.find((person) => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(id).then((person) => {
+          response.json(person)
+      }).catch((error) => {
+          response.status(404).end()
+      }
+    )
 })
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter((person) => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndRemove(id).then(() => {
+        response.status(204).end()
+    }).catch((error) => {
+        console.log(error)
+        response.status(400).send({ error: 'malformatted id' })
+    })
+    
 })
 
 app.post('/api/persons', (request, response) => {
@@ -69,69 +74,16 @@ app.post('/api/persons', (request, response) => {
             error: 'number missing'
         })
     }
-
-    const nameExists = persons.find((person) => person.name === body.name)
-    if (nameExists) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = {
-        id: String(Math.floor(Math.random() * 10000)),
+    const person = new Person({
         name: body.name,
-        number: body.number
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
+        number: body.number,
+    })
+    person.save().then((savedPerson) => {
+        response.json(savedPerson)
+    })
 })
 
-// app.get('/api/notes/:id', (request, response) => {
-//   const id = request.params.id
-//   const note = notes.find((note) => note.id === id)
-
-//   if (note) {
-//     response.json(note)
-//   } else {
-//     response.status(404).end()
-//   }
-// })
-
-// const generateId = () => {
-//   const maxId =
-//     notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
-//   return String(maxId + 1)
-// }
-
-// app.post('/api/notes', (request, response) => {
-//   const body = request.body
-
-//   if (!body.content) {
-//     return response.status(400).json({
-//       error: 'content missing',
-//     })
-//   }
-
-//   const note = {
-//     content: body.content,
-//     important: body.important || false,
-//     id: generateId(),
-//   }
-
-//   notes = notes.concat(note)
-
-//   response.json(note)
-// })
-
-// app.delete('/api/notes/:id', (request, response) => {
-//   const id = request.params.id
-//   notes = notes.filter((note) => note.id !== id)
-
-//   response.status(204).end()
-// })
-
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
