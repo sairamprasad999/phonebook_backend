309bb2d89e08262eb2689e0dea56d4e3bff93e8e
Sai Sanapureddy <ss2644@cornell.edu>
2025-11-19 22:52:36 -0500

    Add frontend distrubition files

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
+app.use(express.static('dist'))
 app.use(express.json())
 
 // Add a token that stringifies the body (only for logging)
